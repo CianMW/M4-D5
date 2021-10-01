839f8a8b3123988cb57e10e6ac17a311ed382f5a
CianMW <markwick.cian@gmail.com>
2021-10-01 11:33:17 +0100

    bootstrap icon error

--- module4-day5/src/App.js
+++ module4-day5/src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomeNavbar from './components/HomeNavbar';
+import HomeSubNav from './components/HomeSubNav'
+import HomeFooter from './components/HomeFooter'
 
 function App() {
   return (
     <div className="App">
       <HomeNavbar />
+     {/*  <HomeSubNav />
+      <HomeFooter /> */}
     </div>
   );
 }
---
+++ module4-day5/src/components/HomeSubNav.jsx
@@ -0,0 +1,104 @@
+import { Container, Row, Col, Dropdown } from "bootstrap"
+
+/* const HomeSubNav = () => {
+    return (
+        <>
+        <><div id="subNav">
+                <div class="row sub-nav">
+                    <div class="col-2">
+                        <h3>TV Show</h3>
+                    </div>
+                    <div class="col-6">
+                        <div class="dropdown">
+                            <button class="btn dropdown-toggle drpdwn" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Genres
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4 mb-lg-2 ">
+                        <div class=" my-2 my-lg-0 text-right">
+                            <a href="#"><img src="assets/icons/text-file-6-48.png" height="30"/></a>
+                            <a href="#"><img src="assets/icons/text-file-6-48.png" height="30"/></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </><div class="container-fluid mt-4 pl-lg-5">
+
+                <h3 class="section-title text-white">Trending Shows</h3>
+                <div class="row mx-n1 row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-6 mb-4">
+                    <div class="col px-1"><img src="assets/media/media0.webp" alt="movie image 0"
+                        class="w-100 img-fluid mb-2 mb-lg-0 rounded"></>
+                    </div>
+                    <div class="col px-1"><img src="assets/media/media1.jpg" alt="movie image 1"
+                        class="w-100 img-fluid mb-2 mb-lg-0 "></>
+                    </div>
+                    <div class="col px-1"><img src="assets/media/media2.webp" alt="movie image 2"
+                        class="w-100 img-fluid mb-2 mb-lg-0 rounded"></>
+                    </div>
+                    <div class="col px-1"><img src="assets/media/media3.webp" alt="movie image 3"
+                        class="w-100 img-fluid mb-2 mb-lg-0 rounded "></>
+                    </div>
+                    <div class="col px-1 d-md-none d-lg-block"><img src="assets/media/media4.jpg" alt="movie image 4"
+                        class="w-100 img-fluid mb-2 mb-lg-0 rounded "></>
+                    </div>
+                    <div class="col px-1 d-md-none d-lg-block"><img src="assets/media/media16.webp" alt="movie image 16"
+                        class="w-100 img-fluid mb-2 mb-lg-0 rounded "></>
+                    </div>
+
+                </div>
+            </div><div class="container-fluid mt-4 pl-lg-5">
+
+                <h3 class="section-title text-white">Watch It Again</h3>
+                <div class="row mx-n1 row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-6 mb-4">
+                    <div class="col px-1"><img src="assets/media/media5.webp" alt="movie image 5"
+                        class="w-100 img-fluid mb-2 mb-lg-0 rounded"></>
+                    </div>
+                    <div class="col px-1"><img src="assets/media/media6.jpg" alt="movie image 6"
+                        class="w-100 img-fluid mb-2 mb-lg-0 "></>
+                    </div>
+                    <div class="col px-1"><img src="assets/media/media7.webp" alt="movie image 7"
+                        class="w-100 img-fluid mb-2 mb-lg-0 rounded"></>
+                    </div>
+                    <div class="col px-1"><img src="assets/media/media8.webp" alt="movie image 8"
+                        class="w-100 img-fluid mb-2 mb-lg-0 rounded "></>
+                    </div>
+                    <div class="col px-1 d-md-none d-lg-block"><img src="assets/media/media9.jpg" alt="movie image 9"
+                        class="w-100 img-fluid mb-2 mb-lg-0 rounded "></>
+                    </div>
+                    <div class="col px-1 d-md-none d-lg-block"><img src="assets/media/media17.jpg" alt="movie image 17"
+                        class="w-100 img-fluid mb-2 mb-lg-0 rounded "></>
+                    </div>
+                </div>
+            </div><div class="container-fluid mt-4 pl-lg-5">
+
+                <h3 class="section-title text-white">New Releases</h3>
+                <div class="row mx-n1 row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-6 mb-4">
+                    <div class="col px-1"><img src="assets/media/media10.jpg" alt="movie image 10"
+                        class="w-100 img-fluid mb-2 mb-lg-0 rounded"></>
+                    </div>
+                    <div class="col px-1"><img src="assets/media/media11.jpg" alt="movie image 11"
+                        class="w-100 img-fluid mb-2 mb-lg-0 "></>
+                    </div>
+                    <div class="col px-1"><img src="assets/media/media12.jpg" alt="movie image 12"
+                        class="w-100 img-fluid mb-2 mb-lg-0 rounded"></>
+                    </div>
+                    <div class="col px-1"><img src="assets/media/media13.jpg" alt="movie image 13"
+                        class="w-100 img-fluid mb-2 mb-lg-0 rounded "></>
+                    </div>
+                    <div class="col px-1 d-md-none d-lg-block"><img src="assets/media/media14.webp" alt="movie image 14"
+                        class="w-100 img-fluid mb-2 mb-lg-0 rounded "></>
+                    </div>
+                    <div class="col px-1 d-md-none d-lg-block"><img src="assets/media/media17.jpg" alt="movie image 17"
+                        class="w-100 img-fluid mb-2 mb-lg-0 rounded "></>
+                    </div>
+                </div>
+            </div></>
+    )
+
+    export default HomeSubNav */
